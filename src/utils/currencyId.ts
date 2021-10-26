0c080153a935d66f39851ae2bc8f990bc47de65a
Gabriele Rigo <gabriele.rigo@gmail.com>
2021-10-26 17:49:54 +0200

    minor bug fix
    - revert to return 'ETH' if currency.isNative in /utils/currencyId in order to prevent returning same value in different childs

--- src/utils/currencyId.ts
+++ src/utils/currencyId.ts
@@ -1,9 +1,7 @@
 import { Currency } from '@uniswap/sdk-core'
 
-import { WETH9_EXTENDED } from '../constants/tokens'
-
 export function currencyId(currency: Currency): string {
-  if (currency.isNative) return WETH9_EXTENDED[currency.chainId].address
+  if (currency.isNative) return 'ETH'
   if (currency.isToken) return currency.address
   throw new Error('invalid currency')
 }
